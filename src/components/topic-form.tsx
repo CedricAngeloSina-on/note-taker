1c4fed536b3456c1b7312b0b2515e008534d4226
CedricAngeloSina-on <cao.sinaon@gmail.com>
2024-09-17 04:08:37 +0800

    feat: topic form insert to db

--- src/components/topic-form.tsx
+++ src/components/topic-form.tsx
@@ -2,6 +2,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { toast } from "sonner";
 
 import { Button } from "~/components/shadcn/button";
 import {
@@ -15,7 +16,7 @@ import {
 } from "~/components/shadcn/form";
 import { Input } from "~/components/shadcn/input";
 
-import { toast } from "sonner";
+import { api } from "~/trpc/react";
 
 const formSchema = z.object({
   topic: z
@@ -25,6 +26,24 @@ const formSchema = z.object({
 });
 
 export default function TopicForm() {
+  const utils = api.useUtils();
+
+  const createTopic = api.topic.create.useMutation({
+    onMutate: () => {
+      toast.loading("Creating new topic...");
+    },
+    onSuccess: async () => {
+      await utils.topic.invalidate();
+      toast.success("New topic created!", { duration: 2000 });
+    },
+    onError: (error) => {
+      toast.error(`Failed to create topic: ${error.message}`);
+    },
+    onSettled: () => {
+      toast.dismiss();
+    },
+  });
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -34,7 +53,9 @@ export default function TopicForm() {
 
   function onSubmit(values: z.infer<typeof formSchema>) {
     console.log(values);
-    toast.success("New topic created!", { duration: 2000 });
+    createTopic.mutate({
+      title: values.topic,
+    });
     form.reset();
   }
 
