42e1f3cc011a37ed2872b37b419ed3c32485a53f
CedricAngeloSina-on <137179865+CedricAngeloSina-on@users.noreply.github.com>
2024-09-18 03:18:26 +0800

    style: nav layout width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ export default async function Home() {
 
   return (
     <HydrateClient>
-      <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+      <div className="grid min-h-screen w-full md:grid-cols-[250px_1fr] lg:grid-cols-[280px_1fr]">
         <div className="hidden border-r bg-muted/40 md:block">
           <div className="flex h-full max-h-screen flex-col gap-2">
             <div className="flex h-14 items-center gap-2 border-b px-4 font-semibold lg:h-[60px] lg:px-6">
